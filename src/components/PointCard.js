cff7f571e9e5c92f587f141cd256b73588c03146
Dewmin Saneru <dewminsaneru.2001@gmail.com>
2024-01-25 23:08:07 +0530

    Initial arrow function in PointCard.js and update card componenrs

--- src/components/PointCard.js
+++ src/components/PointCard.js
@@ -1,21 +1,14 @@
 import React from "react";
 
-function PointCard() {
-  return (
-    <div className="max-w-md w-full rounded overflow-hidden shadow-lg mx-2 my-4">
-      {/* Adjust the width and height of the image as needed */}
-      <img
-        className="w-full h-64 object-cover"
-        src="./assets/donate.png"
-        alt="Card"
-      />
-      <div className="px-6 py-4">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Click Me
-        </button>
-      </div>
+const PointCard = ({ imageUrl, buttonName }) => (
+  <div className="max-w-md w-full rounded overflow-hidden shadow-lg mx-2 my-4">
+    <img className="w-full h-64 object-cover" src={imageUrl} alt="Card" />
+    <div className="px-6 py-4">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        {buttonName}
+      </button>
     </div>
-  );
-}
+  </div>
+);
 
 export default PointCard;
--- src/components/PointSection.js
+++ src/components/PointSection.js
@@ -1,22 +1,36 @@
 import React from "react";
 import PointCard from "./PointCard";
+import imageUrl_1 from "../assets/donate.png"
+import imageUrl_2 from "../assets/request.png"
 
 function PointSection() {
-  return (
-    <>
-        <div className="bg-gray-100 w-full h-screen items-center justify-center ">
-            <div className="text-center">
-                <p className="text-[#000000] font-bold p-2 text-5xl mb-4">
-                    LifeBlood Exchange <br /> Request & Donate
-                </p>
+
+    const card1Props = {
+        imageUrl: imageUrl_2,
+        buttonName: "Request Blood",
+      };
+    
+    const card2Props = {
+        imageUrl: imageUrl_1,
+        buttonName: "Donate Blood",
+      };  
+
+
+    return (
+        <>
+            <div className="bg-gray-100 w-full h-screen items-center justify-center ">
+                <div className="text-center">
+                    <p className="text-[#000000] font-bold p-2 text-5xl mb-4">
+                        LifeBlood Exchange <br /> Request & Donate
+                    </p>
+                </div>
+                <div className=" flex items-center justify-center">
+                    <PointCard{...card1Props}/>
+                    <PointCard{...card2Props}/>
+                </div>
             </div>
-            <div className=" flex items-center justify-center">
-                <PointCard/>
-                <PointCard/>
-            </div>
-        </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default PointSection;
