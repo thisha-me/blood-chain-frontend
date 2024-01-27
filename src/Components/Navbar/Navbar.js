8081f3c0c2613cc5f8030f45f5034ad9d3fb7326
Hiruni Thaksarani <hthaksarani@gmail.com>
2024-01-27 16:32:24 +0530

    Created navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css"
+
+export const Navbar = () => {
+  const navbarRef = useRef();
+
+  const displayNavbar = () => {
+    navbarRef.current.classList.toggle("responsive_navbar");
+  };
+
+  return (
+    <header className="shadow-md flex items-center justify-between h-16 px-8 bg-backgroundColor">
+
+      <p className="text-mainColorLighter font-bold text-2xl">BloodChain</p>
+
+      <nav ref={navbarRef} className="md:flex flex-column gap-8">
+        <a href="/#" className="text-textColor hover:text-mainColorLighter">Home</a>
+        <a href="/#" className="text-textColor hover:text-mainColorLighter">Donate</a>
+        <a href="/#" className="text-textColor hover:text-mainColorLighter">Request</a>
+        <a href="/#" className="login-btn text-textColor ps-5 pe-5 px-1 py-1 bg-mainColorLighter rounded-full hover:bg-mainColor hover:text-backgroundColor">
+            Login</a>
+        <button className="navbar-btn navbar-close-btn md:hidden" onClick={displayNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="navbar-btn md:hidden" onClick={displayNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+};
+
+export default Navbar;
