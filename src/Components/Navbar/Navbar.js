a61851b3441c6f5e863e8c553e61441975cc167b
Hiruni Thaksarani <hthaksarani@gmail.com>
2024-01-28 12:04:42 +0530

    Active links in navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,15 @@
 import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
 import "./Navbar.css"
 
 export const Navbar = () => {
+
+  const [activeNavLink, setActiveNavLink] = useState(null);
+
+  const handleNavLinkClick = (e) => {
+    setActiveNavLink(e.target.href);
+  };
   const navbarRef = useRef();
 
   const displayNavbar = () => {
@@ -15,11 +22,11 @@ export const Navbar = () => {
       <a href="/#" className="text-mainColorLighter font-bold text-2xl">BloodChain</a>
 
       <nav ref={navbarRef} className="md:flex flex-column gap-10">
-        <a href="/#" className="text-textColor hover:text-mainColorLighter">Home</a>
-        <a href="/#" className="text-textColor hover:text-mainColorLighter">Donate</a>
-        <a href="/#" className="text-textColor hover:text-mainColorLighter">Request</a>
+        <a href="/#" className={activeNavLink === "/" ? "text-textColor hover:text-mainColorLighter active:text-red" : "text-textColor hover:text-mainColorLighter"} onClick={handleNavLinkClick}>Home</a>
+        <a href="/donate" className={activeNavLink === "/donate" ? "text-textColor hover:text-mainColorLighter active:text-red" : "text-textColor hover:text-mainColorLighter"} onClick={handleNavLinkClick}>Donate</a>
+        <a href="/request" className={activeNavLink === "/request" ? "text-textColor hover:text-mainColorLighter active:text-red" : "text-textColor hover:text-mainColorLighter"} onClick={handleNavLinkClick}>Request</a>
         <a href="/#" className="login-btn text-backgroundColor ps-5 pe-5 px-1 py-1 bg-mainColorLighter rounded-full hover:bg-mainColor hover:text-backgroundColor">
-            Login</a>
+          Login</a>
         <button className="navbar-btn navbar-close-btn md:hidden" onClick={displayNavbar}>
           <FaTimes />
         </button>
