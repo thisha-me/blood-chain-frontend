dbce586ab6fbecb5e299d55c95fc89be1cac9368
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-01-30 10:56:19 +0530

    Login page
    Edited

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,25 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Login = () => {
-  // State variables to manage the email input value and its validity  
-  const [email, setEmail] = useState("");
-  const [isEmailValid, setIsEmailValid] = useState(true);
-
-  // Function to handle changes in the email input
-  const handleEmailChange = (e) => {
-    // Get the entered email from the input
-    const enteredEmail = e.target.value;
-    // Update the email state
-    setEmail(enteredEmail);
-
-    //this checks the email for @ sign
-    setIsEmailValid(enteredEmail.includes("@"));
-  };
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="p-8 bg-white rounded-md shadow-md">
-        <h1 className="text-center text-4xl font-bold mb-4 p-2 bg-red-700 text-white rounded-md w-40 mx-auto">
+      <div className="p-8 bg-white rounded-md shadow-md"> 
+
+      {/* page title */}
+        <h1  className="text-center text-xl font-bold mb-4 p-2 bg-red-700 text-white rounded-md w-40 mx-auto">
           BloodChain
         </h1>
         <p className="text-center text-xl font-bold mb-4 p-2 text-gray-800">
@@ -29,50 +16,36 @@ const Login = () => {
           Login to your account :
         </p>
 
-        
+      {/* Email box */}
         <form className="flex flex-col">
-          {/* email input */}
-          <input
-            className={`border mb-4 text-center bg-white hover:bg-gray-700 text-black py-3 rounded w-60 text-1xl ${
-              !isEmailValid && "border-red-500"
-            }`}
-            type="text"
-            name="email"
-            placeholder="Email"
-            value={email}
-            onChange={handleEmailChange}
-          />
+        <input className=" border mb-4 text-center bg-white text-black  py-3 rounded w-60  text-1xl "
+               type="text"
+               name="email"
+               placeholder="Email"/>
 
-          {/* display error message for invalid inputs */}
-          {!isEmailValid && (
-            <p className="text-left  mb-4">
-              Invalid email address
-            </p>
-          )}
+      {/* password box */}
+          <input className="border mb-4 text-center bg-white  text-black  py-3 rounded w-60  text-1xl"
+                 type="password"
+                 name="password"
+                 placeholder="Password"/>
 
-          {/* password input */}
-          <input
-            className="border mb-4 text-center bg-white hover:bg-gray-700 text-black py-3 rounded w-60 text-1xl"
-            type="password"
-            name="password"
-            placeholder="Password"/>
-
+      {/* checkbox */}
           <div className="flex items-center mb-4">
-            <input type="checkbox" id="rememberMe" className="mr-2" />
+            <input type="checkbox"
+                   id="rememberMe"
+                   className="mr-2"/>
             <label htmlFor="rememberMe" className="text-sm text-gray-600 p-2">
               Remember me
             </label>
           </div>
-
-          <a
-            href="/signin"
-            className="text-center text-4xl font-bold mb-14 p-2 bg-red-700 text-white rounded-md w-40 mx-auto">
+    
+          <a href="/signin" className="text-center text-xl font-bold mb-14 p-2 bg-red-700 text-white rounded-md w-40 mx-auto">
             Sign In
           </a>
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default Login;
--- src/Pages/LoginForm.js
+++ src/Pages/LoginForm.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const LoginForm = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center ">
+    <div className="min-h-screen flex items-center justify-center  ">
       <div className="p-8 bg-white rounded-md shadow-md">
         <h1
-          className="text-center text-4xl font-bold mb-14 p-2 bg-red-700 text-white rounded-md w-40 mx-auto">
+          className="text-center text-xl font-bold mb-14 p-2 bg-red-700 text-white rounded-md w-40 mx-auto">
           BloodChain
         </h1>
         <div className="flex flex-col space-y-4 items-center">
@@ -13,7 +13,7 @@ const LoginForm = () => {
         {/* Login and registration buttons */}
           <a
             href="/register"
-            className="bg-white hover:bg-red-700 text-black font-bold py-3 rounded w-60 mb-4 text-center  text-2xl shadow-md">
+            className="bg-white hover:bg-red-700 text-black font-bold py-3 rounded w-60 mb-4 text-center  text-xl shadow-md">
             Registration
           </a>
 
@@ -21,7 +21,7 @@ const LoginForm = () => {
 
           <a
             href="/login"
-            className="bg-white hover:bg-red-700 text-black font-bold py-3 rounded w-60 text-center text-2xl shadow-md">
+            className="bg-white hover:bg-red-700 text-black font-bold py-3 rounded w-60 text-center text-xl shadow-md">
             Login
           </a>
         </div>
