433bbcf920387d1ad51d45ce4785a1c0595a7e26
Hiruni Thaksarani <hthaksarani@gmail.com>
2024-01-30 10:27:43 +0530

    Add CSS to donation cards

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './output.css';
-import App from './App';
+import DonationCards from './Pages/DonationCards';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DonationCards/>
   </React.StrictMode>
 );
 
