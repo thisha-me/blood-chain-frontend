1c2aba1d6c0bad0410f53ae287ad5c556752d6fa
Hiruni Thaksarani <hthaksarani@gmail.com>
2024-02-04 22:59:35 +0530

    Update login button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Navbar from './Components/Navbar/Navbar';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,33 +1,77 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { useState } from "react";
 import "./Navbar.css";
 
-export const Navbar = () => {
-
+const Navbar = () => {
   const [activeNavLink, setActiveNavLink] = useState(null);
+  const [showShadow, setShowShadow] = useState(false);
 
   const handleNavLinkClick = (e) => {
     setActiveNavLink(e.target.href);
   };
+
   const navbarRef = useRef();
 
   const displayNavbar = () => {
     navbarRef.current.classList.toggle("responsive_navbar");
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setShowShadow(scrollPosition > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="shadow-md flex items-center justify-between h-14 px-10 bg-backgroundColor">
+    <header
+      className={`flex items-center justify-between h-12 mt-1 py-2 px-10 bg-backgroundColor ${
+        showShadow ? "shadow-md" : ""
+      }`}
+    >
+      <a href="/#" className="text-mainColorLighter font-bold text-3xl">BloodChain</a>
 
-      <a href="/#" className="text-mainColorLighter font-bold text-2xl">BloodChain</a>
+      <nav ref={navbarRef} className="md:flex flex-column gap-10 ">
+        <a href="/#"
+          className={`${
+            activeNavLink === "/"
+              ? "text-textColor hover:text-mainColorLighter active:text-red "
+              : "text-textColor hover:text-mainColorLighter"
+          }`}
+          onClick={handleNavLinkClick}
+        >Home</a>
 
-      <nav ref={navbarRef} className="md:flex flex-column gap-10">
-        <a href="/#" className={activeNavLink === "/" ? "text-textColor hover:text-mainColorLighter active:text-red" : "text-textColor hover:text-mainColorLighter"} onClick={handleNavLinkClick}>Home</a>
-        <a href="/donate" className={activeNavLink === "/donate" ? "text-textColor hover:text-mainColorLighter active:text-red" : "text-textColor hover:text-mainColorLighter"} onClick={handleNavLinkClick}>Donate</a>
-        <a href="/request" className={activeNavLink === "/request" ? "text-textColor hover:text-mainColorLighter active:text-red" : "text-textColor hover:text-mainColorLighter"} onClick={handleNavLinkClick}>Request</a>
-        <a href="/#" className="login-btn text-backgroundColor ps-5 pe-5 px-1 py-1 bg-mainColorLighter rounded-full hover:bg-mainColor hover:text-backgroundColor">
-          Login</a>
-        <button className="navbar-btn navbar-close-btn md:hidden" onClick={displayNavbar}>
+        <a href="/donate"
+          className={`${
+            activeNavLink === "/donate"
+              ? "text-textColor hover:text-mainColorLighter active:text-red"
+              : "text-textColor hover:text-mainColorLighter"
+          }`}
+          onClick={handleNavLinkClick}
+        >Donate</a>
+
+        <a href="/request"
+          className={`${
+            activeNavLink === "/request"
+              ? "text-textColor hover:text-mainColorLighter active:text-red"
+              : "text-textColor hover:text-mainColorLighter"
+          }`}
+          onClick={handleNavLinkClick}
+        >Request</a>
+
+        <a href="/#" className="login-btn text-backgroundColor px-5 bg-mainColorLighter rounded-full hover:bg-mainColor hover:text-backgroundColor "
+        >Login</a>
+
+        <button
+          className="navbar-btn navbar-close-btn md:hidden"
+          onClick={displayNavbar}
+        >
           <FaTimes />
         </button>
       </nav>
