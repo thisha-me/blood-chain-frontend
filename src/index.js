1a7774279c1813af490d4c8c1faa5da5c72f07f9
Hiruni Thaksarani <hthaksarani@gmail.com>
2024-02-07 22:22:43 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './output.css';
-import App from './App';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <nav />
+    <Navbar/>
+    <Footer/>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
