6bc162f0aff335e4032425909e74f87d0c93ff8e
Dewmin Saneru <dewminsaneru.2001@gmail.com>
2024-02-10 20:07:16 +0530

    add some responsive changes

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -29,41 +29,53 @@ const AboutSection = () => {
     return (
         <div className="min-h-screen flex items-center justify-center ">
             <div className= {`flex ${isMobile ? "flex-col" : "flex"} items-center justify-center`}>
-                <div className="flex-col items-center justify-center">
-                    <div className=" mx-0  px-6 py-0 flex">
-                            <div style={{maxWidth: '400px', height: '250px', width: '250px' }} className=" bg-white max-w-md w-full rounded-xl overflow-hidden shadow-xl mx-2 my-4 flex flex-col justify-center items-center hover:scale-105 transition-all duration-500">
-                                <img src={Image_1} alt="Free Forever" className="" />
-                                <div>
-                                    <p className="text-[#DC143C] font-normal p-2 text-3xl">Free Forever</p>
-                                </div>
-                            </div>
-                            <div style={{maxWidth: '400px', height: '250px', width: '250px' }} className=" bg-white max-w-md w-full rounded-xl overflow-hidden shadow-xl mx-2 my-4 flex flex-col justify-center items-center hover:scale-105 transition-all duration-500">
-                                <img src={Image_2} alt="Save a Life" className="" />
-                                <div>
-                                    <p className="text-[#DC143C] font-normal p-2 text-3xl">Save a Life</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className=" mx-0  px-6 py-0 flex">
-                            <div style={{maxWidth: '400px', height: '250px', width: '250px' }} className=" bg-white max-w-md w-full rounded-xl overflow-hidden shadow-xl mx-2 my-4 flex flex-col justify-center items-center hover:scale-105 transition-all duration-500">
-                                <img src={Image_3} alt="Free Forever" className="" />
-                                <div>
-                                    <p className="text-[#DC143C] font-normal p-2 text-3xl">Free Forever</p>
-                                </div>
-                            </div>
-                            <div style={{maxWidth: '400px', height: '250px', width: '250px' }} className=" bg-white max-w-md w-full rounded-xl overflow-hidden shadow-xl mx-2 my-4 flex flex-col justify-center items-center hover:scale-105 transition-all duration-500">
-                                <img src={Image_4} alt="Save a Life" className="" />
-                                <div>
-                                    <p className="text-[#DC143C] font-normal p-2 text-3xl">Save a Life</p>
-                                </div>
-                            </div>
-                        </div>
+            <div className="flex-col items-center justify-center">
+                <div className="mx-0 px-6 py-0 flex flex-wrap">
+                    <div
+                    style={{ maxWidth: '400px', height: '200px', width: '200px' }}
+                    className="bg-white max-w-md w-full rounded-xl overflow-hidden shadow-xl mx-2 my-4 flex flex-col justify-center items-center hover:scale-105 transition-all duration-500"
+                    >
+                    <img src={Image_1} alt="Free Forever" className="" />
+                    <div>
+                        <p className="text-[#DC143C] font-normal p-2 text-3xl">Find Blood</p>
                     </div>
+                    </div>
+                    <div
+                    style={{ maxWidth: '400px', height: '200px', width: '200px' }}
+                    className="bg-white max-w-md w-full rounded-xl overflow-hidden shadow-xl mx-2 my-4 flex flex-col justify-center items-center hover:scale-105 transition-all duration-500"
+                    >
+                    <img src={Image_2} alt="Save a Life" className="" />
+                    <div>
+                        <p className="text-[#DC143C] font-normal p-2 text-3xl">Get Notified</p>
+                    </div>
+                    </div>
+                </div>
+                <div className="mx-0 px-6 py-0 flex flex-wrap">
+                    <div
+                    style={{ maxWidth: '400px', height: '200px', width: '200px' }}
+                    className="bg-white max-w-md w-full rounded-xl overflow-hidden shadow-xl mx-2 my-4 flex flex-col justify-center items-center hover:scale-105 transition-all duration-500"
+                    >
+                    <img src={Image_3} alt="Free Forever" className="" />
+                    <div>
+                        <p className="text-[#DC143C] font-normal p-2 text-3xl">Free Forever</p>
+                    </div>
+                    </div>
+                    <div
+                    style={{ maxWidth: '400px', height: '200px', width: '200px' }}
+                    className="bg-white max-w-md w-full rounded-xl overflow-hidden shadow-xl mx-2 my-4 flex flex-col justify-center items-center hover:scale-105 transition-all duration-500"
+                    >
+                    <img src={Image_4} alt="Save a Life" className="" />
+                    <div>
+                        <p className="text-[#DC143C] font-normal p-2 text-3xl">Save a Life</p>
+                    </div>
+                    </div>
+                </div>
+        </div>
                 <div className="">
                     <div className="max-w-lg w-full rounded overflow-hidden mx-2 flex flex-col justify-center items-center">
                         <p className="text-[#000000] font-bold p-2 text-5xl hover:scale-105 transition-all duration-500">About <span className="text-[#DC143C]">Blood Chain</span></p>
                         <div>
-                        <p className="text-[#000000] font-normal p-2 text-2xl text-justify">Transforming donation landscapes. Decentralized platform revolutionizes blood & organ access. Secure data, empowered patients, transparent impact. <span className=" font-bold">Join the life-saving chain.</span><br /></p>
+                        <p style={{fontSize: '18px'}} className="text-[#000000] font-normal p-2 text-justify">Transforming donation landscapes. Decentralized platform revolutionizes blood & organ access. Secure data, empowered patients, transparent impact. <span className=" font-bold">Join the life-saving chain.</span><br /></p>
                             <button className="bg-[#8C0909] hover:bg-red-700 text-white font-bold py-2 px-4 rounded-xl">Read More</button>
                             <br/><br/>
                             <hr className="border-t-2 border-red-500"/>
@@ -71,10 +83,10 @@ const AboutSection = () => {
                     </div>
                     <div className=" max-w-lg w-full rounded overflow-hidden mx-2 my-4 flex items-center">
                         <div className="w-full">
-                            <p className="text-[#DC143C] font-bold p-2 text-5xl">240+</p>
-                            <p className="text-[#000000] font-normal p-2 text-3xl">Fulfilled Requests</p>
-                            <p className="text-[#DC143C] font-bold p-2 text-5xl">10+</p>
-                            <p className="text-[#000000] font-normal p-2 text-3xl">Pending Requests</p>
+                            <p style={{fontSize: '40px'}} className="text-[#DC143C] font-bold p-1">240+</p>
+                            <p style={{fontSize: '25px'}} className="text-[#000000] font-medium p-1 text-3xl">Fulfilled Requests</p>
+                            <p style={{fontSize: '40px'}} className="text-[#DC143C] font-bold p-1">10+</p>
+                            <p style={{fontSize: '25px'}} className="text-[#000000] font-medium p-1 text-3xl">Pending Requests</p>
                         </div>
                         <img src={Image_5} alt="Save a Life" />
                     </div>
--- src/components/InstructionSection.js
+++ src/components/InstructionSection.js
@@ -31,31 +31,31 @@ const InstructionSection = () => {
       </div>
       <div className= {`flex ${isMobile ? "flex-col" : "flex"} items-center justify-between`}>
         <div className="max-w-md w-full">
-          <img src={imageUrl} alt="How it works" />
+          <img style={{height: '350px'}} src={imageUrl} alt="How it works" />
         </div>
         
         <div className=" max-w-lg  w-full">
-          <p style={{ fontSize: '1.4rem' }} className="text-[#000000] font-normal p-2  text-justify">Once a match is made, our platform securely connects you directly with the donor or recipient. Communicate details and arrange the blood transfer without intermediaries.</p>
+          <p style={{fontSize: '18px'}} className="text-[#000000] font-medium p-2  text-justify">Once a match is made, our platform securely connects you directly with the donor or recipient. Communicate details and arrange the blood transfer without intermediaries.</p>
           <ul className="list-disc pl-6 leading-loose">
             <li className="flex items-center">
               <img src={icon} alt="Register Icon" />
-              <p style={{ fontSize: '1.4rem' }} className="mr-2 text-[#000000] text-2xl font-normal p-2">Register</p>
+              <p style={{ fontSize: '1.3rem' }} className="mr-2 text-[#000000] text-2xl font-normal p-2">Register</p>
             </li>
             <li className="flex items-center">
               <img src={icon} alt="Profile Icon" />
-              <p style={{ fontSize: '1.4rem' }} className="mr-2 text-[#000000] text-2xl font-normal p-2">Fill your Profile</p>
+              <p style={{ fontSize: '1.3rem' }} className="mr-2 text-[#000000] text-2xl font-normal p-2">Fill your Profile</p>
             </li>
             <li className="flex items-center">
               <img src={icon} alt="Request Icon" />
-              <p style={{ fontSize: '1.4rem' }} className="mr-2 text-[#000000] text-2xl font-normal p-2">Request Blood</p>
+              <p style={{ fontSize: '1.3rem' }} className="mr-2 text-[#000000] text-2xl font-normal p-2">Request Blood</p>
             </li>
             <li className="flex items-center">
               <img src={icon} alt="Donate Icon" />
-              <p className="mr-2 text-[#000000] text-2xl font-normal p-2">Donate Blood</p>
+              <p style={{ fontSize: '1.3rem' }} className="mr-2 text-[#000000] text-2xl font-normal p-2">Donate Blood</p>
             </li>
             <li className="flex items-center">
               <img src={icon} alt="View Icon" />
-              <p style={{ fontSize: '1.4rem' }} className="mr-2 text-[#000000] text-2xl font-normal p-2">View pending Requests</p>
+              <p style={{ fontSize: '1.3rem' }} className="mr-2 text-[#000000] text-2xl font-normal p-2">View pending Requests</p>
             </li>
           </ul>
           <br/>
