907980d1b10b7a3ca51bbe1108433981ccdc595a
thenukadinudaya <thenukagurusinghe0@gmail.com>
2024-02-17 22:25:47 +0530

    commented the button in output.css so the register button is visible

--- src/components/renderingPage.js
+++ src/components/renderingPage.js
@@ -23,6 +23,9 @@ const RenderingPage = () => {
                     <button type='submit' className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:bg-red-600 mx-auto block mt-4">
                         Register
                     </button>
+
+
+
                 </form>
             </div>
         </div>
