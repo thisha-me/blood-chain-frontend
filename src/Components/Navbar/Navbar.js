3f5cfd9dc1f294d503abff20e4c57f6af941357e
Hiruni Thaksarani <hthaksarani@gmail.com>
2024-03-21 18:24:59 +0530

    Changed the connect button color

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,16 +3,28 @@ import { useRef, useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
 import { Link, useLocation } from "react-router-dom";
-import { ConnectWallet, Web3Button, useAddress, useContract, useContractWrite, useContractRead, useDisconnect } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  Web3Button,
+  useAddress,
+  useContract,
+  useContractWrite,
+  useContractRead,
+  useDisconnect,
+} from "@thirdweb-dev/react";
 import { STATUS_CONTRACT_ADDRESS } from "../Navbar/../../constants/addresses";
 
 const Navbar = () => {
-
   const address = useAddress();
   const [newStatus, setNewStatus] = useState(address);
-
-  const { contract } = useContract("0x729676943630Cc1090a10100Db0E55ee0EAc33b4");
-  const { mutateAsync: connectWallet, isLoading } = useContractWrite(contract, "connectWallet")
+  
+  const { contract } = useContract(
+    "0x729676943630Cc1090a10100Db0E55ee0EAc33b4"
+  );
+  const { mutateAsync: connectWallet, isLoading } = useContractWrite(
+    contract,
+    "connectWallet"
+  );
 
   const call = async () => {
     try {
@@ -21,12 +33,11 @@ const Navbar = () => {
     } catch (err) {
       console.error("contract call failure", err);
     }
-  }
+  };
 
   // const connectWallet = async () => {
   //   console.log('Connected to MetaMask:', address);
-  
-    
+
   // };
 
   const [activeNavLink, setActiveNavLink] = useState(null);
@@ -64,48 +75,52 @@ const Navbar = () => {
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 flex items-center justify-between h-13 py-2 px-10 bg-backgroundColor ${showShadow ? "shadow-md" : ""
-        }`}
+      className={`fixed top-0 left-0 right-0 z-50 flex items-center justify-between h-13 py-2 px-10 bg-backgroundColor ${
+        showShadow ? "shadow-md" : ""
+      }`}
     >
-      <Link to="/" className="text-mainColorLighter font-bold text-3xl"
+      <Link
+        to="/"
+        className="text-mainColorLighter font-bold text-3xl"
         onClick={() => handleNavLinkClick("/")}
       >
         BloodChain
       </Link>
 
-      <nav ref={navbarRef} className="md:flex flex-column gap-8 ">
+      <nav ref={navbarRef} className="md:flex flex-column gap-8 flex items-center">
         <Link
           to="/"
-          className={`${activeNavLink === "/"
-            ? "text-textColor hover:text-mainColorLighter active-text-red"
-            : "text-textColor hover:text-mainColorLighter"
-            }`}
+          className={`${
+            activeNavLink === "/"
+              ? "text-textColor hover:text-mainColorLighter active-text-red"
+              : "text-textColor hover:text-mainColorLighter"
+          }`}
           onClick={() => handleNavLinkClick("/")}
           onClickCapture={displayNavbar}
-
         >
           Home
         </Link>
 
         <Link
           to="/donate"
-          className={`${activeNavLink === "/donate"
-            ? "text-textColor hover:text-mainColorLighter active-text-red"
-            : "text-textColor hover:text-mainColorLighter"
-            }`}
+          className={`${
+            activeNavLink === "/donate"
+              ? "text-textColor hover:text-mainColorLighter active-text-red"
+              : "text-textColor hover:text-mainColorLighter"
+          }`}
           onClick={() => handleNavLinkClick("/donate")}
           onClickCapture={displayNavbar}
-
         >
           Donate
         </Link>
 
         <Link
           to="/request"
-          className={`${activeNavLink === "/request"
-            ? "text-textColor hover:text-mainColorLighter active-text-red"
-            : "text-textColor hover:text-mainColorLighter"
-            }`}
+          className={`${
+            activeNavLink === "/request"
+              ? "text-textColor hover:text-mainColorLighter active-text-red"
+              : "text-textColor hover:text-mainColorLighter"
+          }`}
           onClick={() => handleNavLinkClick("/request")}
           onClickCapture={displayNavbar}
         >
@@ -114,33 +129,31 @@ const Navbar = () => {
 
         <Link
           to="/profile"
-          className={`${activeNavLink === "/profile"
-            ? "text-textColor hover:text-mainColorLighter active-text-red"
-            : "text-textColor hover:text-mainColorLighter"
-            }`}
+          className={`${
+            activeNavLink === "/profile"
+              ? "text-textColor hover:text-mainColorLighter active-text-red"
+              : "text-textColor hover:text-mainColorLighter"
+          }`}
           onClick={() => handleNavLinkClick("/profile")}
           onClickCapture={displayNavbar}
-
         >
           Profile
         </Link>
 
         <div className="connect-wallet-button">
-        <Link
-          onClick={() => {
-            call();
-            
-          }}
-          onClickCapture={displayNavbar}
-
-        >
-          <ConnectWallet
-            theme={"light"}
-            modalSize={"wide"}
-          />
-        </Link>
+          <Link
+            onClick={() => {
+              call();
+            }}
+            onClickCapture={displayNavbar}
+          >
+            <ConnectWallet theme={"light"} 
+            modalSize={"wide"} 
+            style={{ height: "50px" }}
+            />
+          </Link>
+        
         </div>
-
         <button
           className="navbar-btn navbar-close-btn md:hidden"
           onClick={displayNavbar}
@@ -151,6 +164,7 @@ const Navbar = () => {
       <button className="navbar-btn md:hidden" onClick={displayNavbar}>
         <FaBars />
       </button>
+
     </header>
   );
 };
