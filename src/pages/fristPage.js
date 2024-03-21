1d58e6cc8fdc7d4baf662507558fb1f01b0aadfc
Hiruni Thaksarani <hthaksarani@gmail.com>
2024-03-21 13:42:22 +0530

    changed the z index of the fullfil popup

--- src/pages/fristPage.js
+++ src/pages/fristPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import '../App.css';
 import AboutSection from '../Components/AboutSection';
 import InstructionSection from '../Components/InstructionSection';
