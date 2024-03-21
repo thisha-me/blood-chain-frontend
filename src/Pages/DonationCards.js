94ed5b74eb7442685788a35bd14cbc5b56f4a431
Hiruni Thaksarani <hthaksarani@gmail.com>
2024-03-21 21:31:37 +0530

    add loading function

--- src/Pages/DonationCards.js
+++ src/Pages/DonationCards.js
@@ -1,137 +1,42 @@
-import React, { useState } from "react";
+/* eslint-disable no-undef */
+import React, { useState, useEffect } from "react";
 import donateBlood from "../Images/Rectangle 53.png";
 import "../Styles/DonationCard.css";
 import statement from "../Images/statement.png";
 import { useContract, useContractRead } from "@thirdweb-dev/react";
 
 const DonationCards = () => {
-
-  //===================================================================================================
-  //fetching data from blockchain for the donation cards
   const { contract } = useContract("0x1C8b6ace2BD3f9A5007c1cf0b06eE531ad3Dd17A");
-  const { data: bloodRequestData, isLoading : loading } = useContractRead(contract, "getAllRequests");
-  console.log("Wallet Addresses:", bloodRequestData);
-  
-  let formattedData = []; // Declare formattedData outside of the if block
-
-  if(loading){
-    console.log("loading...")
-  } else {
-    formattedData = bloodRequestData.map((request) => {
-      const [id, name, contactNumber, location, , donationCenter, bloodType, dateTimeObj, ] = request;
-      // Extract date and time from the BigNumber hex value
-      const date  = new Date(dateTimeObj.toNumber() * 1000);
-      const dateString = date.toLocaleDateString('en-US');
-      const timeString = date.toLocaleTimeString('en-US', {
-        hour: '2-digit',
-        minute: '2-digit'
-      });
-    
-      return {
-        id: id,
-        name: name,
-        location: location,
-        bloodType: bloodType,
-        date: dateString,
-        time: timeString,
-        donationCenter: donationCenter,
-        contactNumber: contactNumber,
-      };
-    });
-
-    // To display the formatted data in the console
-    console.log(formattedData);
-  }
-
-  // if (bloodRequestData) {
-  //   formattedData = bloodRequestData.map((request) => {
-  //     const [id, name, contactNumber, location, , donationCenter, bloodType, dateTimeObj, ] = request;
-  //     // Extract date and time from the BigNumber hex value
-  //     const date  = new Date(dateTimeObj.toNumber() * 1000);
-  //     const dateString = date.toLocaleDateString('en-US');
-  //     const timeString = date.toLocaleTimeString('en-US', {
-  //       hour: '2-digit',
-  //       minute: '2-digit'
-  //     });
-    
-  //     return {
-  //       id: id,
-  //       name: name,
-  //       location: location,
-  //       bloodType: bloodType,
-  //       date: dateString,
-  //       time: timeString,
-  //       donationCenter: donationCenter,
-  //       contactNumber: contactNumber,
-  //     };
-  //   });
-
-  //   // To display the formatted data in the console
-  //   console.log(formattedData);
-  // } else {
-  //   console.log('Waiting for data...');
-  // }
-  //===================================================================================================
-
-  const [selectedRequest, setRequest] = useState(null);
+  const { data: bloodRequestData, isLoading: loading } = useContractRead(contract, "getAllRequests");
+  const [formattedData, setFormattedData] = useState([]);
+  const [showLoader, setShowLoader] = useState(true); // State to manage loader visibility
   const [showPopup, setShowPopup] = useState(false);
-  const [selectedBloodType, setBloodType] = useState(null);
-  const [sortedRequests, setSortedRequests] = useState([...formattedData]);
-  const [isAscending, setIsAscending] = useState(true);
-
-  const filterRequests = () => {
-    if (selectedBloodType) {
-      return sortedRequests.filter(
-        (request) => request.bloodType === selectedBloodType
-      );
-    }
-    return sortedRequests;
-  };
-
-  const convertToDateObject = (dateString, timeString) => {
-    const dateParts = dateString.split("/");
-    const timeParts = timeString.split(" ");
-    const year = parseInt(dateParts[2]);
-    const month = parseInt(dateParts[1]) - 1;
-    const day = parseInt(dateParts[0]);
-    const hours = parseInt(timeParts[0].split(":")[0]);
-    const minutes = parseInt(timeParts[0].split(":")[1]);
-    return new Date(year, month, day, hours, minutes);
-  };
-
-  const sortRequestsByDateAndTime = () => {
-    const sortedData = [...sortedRequests];
-
-    sortedData.sort((a, b) => {
-      const dateA = convertToDateObject(a.date, a.time);
-      const dateB = convertToDateObject(b.date, b.time);
-      return dateA - dateB;
-    });
-
-    if (isAscending) {
-      sortedData.sort((a, b) => {
-        const timeA = new Date(a.time).getTime();
-        const timeB = new Date(b.time).getTime();
-
-        return timeA - timeB;
+  
+  useEffect(() => {
+    if (!loading && bloodRequestData) {
+      const formatted = bloodRequestData.map((request) => {
+        const [id, name, contactNumber, location, , donationCenter, bloodType, dateTimeObj] = request;
+        const date = new Date(dateTimeObj.toNumber() * 1000);
+        const dateString = date.toLocaleDateString('en-US');
+        const timeString = date.toLocaleTimeString('en-US', {
+          hour: '2-digit',
+          minute: '2-digit'
+        });
+        return {
+          id: id,
+          name: name,
+          location: location,
+          bloodType: bloodType,
+          date: dateString,
+          time: timeString,
+          donationCenter: donationCenter,
+          contactNumber: contactNumber,
+        };
       });
-    } else {
-      sortedData.sort((a, b) => {
-        const timeA = new Date(a.time).getTime();
-        const timeB = new Date(b.time).getTime();
-
-        return timeB - timeA;
-      });
+      setFormattedData(formatted);
+      setShowLoader(false); // Once data is loaded, hide the loader
     }
-
-    setSortedRequests(sortedData);
-
-    setIsAscending(!isAscending);
-  };
-
-  const resetSorting = () => {
-    setSortedRequests([...formattedData]);
-  };
+  }, [bloodRequestData, loading]);
 
   return (
     <div className="py-1 px-4 md:px-10 lg:px-20 mt-16 mb-16">
@@ -148,65 +53,48 @@ const DonationCards = () => {
         />
       </div>
 
-      <div className="mb-10 flex flex-col md:flex-row items-center justify-center md:w-auto gap-32">
-        <p className="text-4xl font-bold  md:mb-20 md:w-auto">
-          Currently Received Requests
-        </p>
+      {/* Loader */}
+      {showLoader && <div className="text-center">Loading...</div>}
 
-        <div className="grid grid-cols-1 md:grid-cols-1 gap-5 md:w-auto">
-          <button
-            className=" button text-backgroundColor flex min-w-[200px] justify-content: flex-start align-items: center px-4 py-2 bg-primaryColor hover:secondaryColor text-bgColor1 rounded mx-2 max-h-[38px]"
-            onClick={sortRequestsByDateAndTime}
-            onDoubleClick={resetSorting}
-          >
-            Sort by Date and Time
-          </button>
-
-          <select
-            value={selectedBloodType}
-            onChange={(e) => setBloodType(e.target.value)}
-            className="border border-bgColor2 px-2 py-1 rounded mx-2 md:w-auto min-w-[200px]"
-          >
-            <option disabled selected>
-              Select Blood Type
-            </option>
-            <option value="">ALL</option>
-            <option value="A+">A+</option>
-            <option value="A-">A-</option>
-            <option value="B+">B+</option>
-            <option value="B-">B-</option>
-            <option value="AB+">AB+</option>
-            <option value="AB-">AB-</option>
-            <option value="O+">O+</option>
-            <option value="O-">O-</option>
-          </select>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-10 px-20 md:px-20 lg:px-56 mb-10">
-        {filterRequests().map((request) => (
-          <div key={request.id} className="shadow-md border rounded-lg p-4">
-            <p className="font-bold">Request ID : {request.id}</p>
-            <p>Name : {request.name}</p>
-            <p>Location : {request.location}</p>
-            <p className="text-center">Blood Type </p>
-            <p className="text-center font-extrabold">{request.bloodType} </p>
-            <div className="text-center">
-              <button
-                onClick={() => {
-                  setRequest(request);
-                  setShowPopup(true);
-                }}
-                className="my-3 px-4 py-2 button text-backgroundColor  rounded-lg "
-              >
-                Donate
-              </button>
-            </div>
-            <p className="text-right text-xs">Time : {request.time}</p>
-            <p className="text-right text-xs">Date : {request.date}</p>
+      {/* Render donation cards when data is loaded */}
+      {!showLoader && (
+        <div className="mb-10 flex flex-col md:flex-row items-center justify-center md:w-auto gap-32">
+          <p className="text-4xl font-bold  md:mb-20 md:w-auto">
+            Currently Received Requests
+          </p>
+          <div className="grid grid-cols-1 md:grid-cols-1 gap-5 md:w-auto">
+            {/* Your sorting and filtering buttons */}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
+
+      {/* Render donation cards when data is loaded */}
+      {!showLoader && (
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-10 px-20 md:px-20 lg:px-56 mb-10">
+          {formattedData.map((request) => (
+            <div key={request.id} className="shadow-md border rounded-lg p-4">
+              <p className="font-bold">Request ID : {request.id}</p>
+              <p>Name : {request.name}</p>
+              <p>Location : {request.location}</p>
+              <p className="text-center">Blood Type </p>
+              <p className="text-center font-extrabold">{request.bloodType} </p>
+              <div className="text-center">
+                <button
+                  onClick={() => {
+                    setRequest(request);
+                    setShowPopup(true);
+                  }}
+                  className="my-3 px-4 py-2 button text-backgroundColor  rounded-lg "
+                >
+                  Donate
+                </button>
+              </div>
+              <p className="text-right text-xs">Time : {request.time}</p>
+              <p className="text-right text-xs">Date : {request.date}</p>
+            </div>
+          ))}
+        </div>
+      )}
 
       {showPopup && (
         <div className="fixed top-0 left-0 w-full h-full bg-secondaryColor bg-opacity-60 flex items-center justify-center">
