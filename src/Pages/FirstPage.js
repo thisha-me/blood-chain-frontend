130bee01e7ee88f9c7ff2f6f1ac1d1534b7d7296
Dhanuja Thishakya Samaranayake <d.thishakya@gmail.com>
2024-03-22 22:55:12 +0530

    update about section

--- src/Pages/FirstPage.js
+++ src/Pages/FirstPage.js
@@ -1,5 +1,5 @@
 import '../App.css';
-import AboutSection from '../Components/About';
+import AboutSection from '../Components/AboutSection';
 import InstructionSection from '../Components/InstructionSection';
 import PointSection from '../Components/PointSection';
 import RegistrationSection from '../Components/RegistrationSection';
