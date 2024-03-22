6827db78b8ace589587d35e88c0ed3a909603e93
Dewmin Saneru <dewminsaneru.2001@gmail.com>
2024-03-22 12:11:01 +0530

    add email to the registration form

--- src/Pages/RegistrationForm.js
+++ src/Pages/RegistrationForm.js
@@ -5,6 +5,7 @@ const RegistrationForm = () => {
     const [formData, setFormData] = useState({
         username : '',
         contactNum : '',
+		email : '',
         age: '',
         bloodType : ''
     });
@@ -59,6 +60,19 @@ const RegistrationForm = () => {
 
 					<div className="mb-4">
 						<label className="block text-sm font-medium text-gray-600">
+							Email:
+						</label>
+						<input
+							type="text"
+							name="email"
+							value={formData.email}
+							onChange={handleChange}
+							className="mt-1 p-2 w-full border rounded-md"
+						/>
+					</div>
+
+					<div className="mb-4">
+						<label className="block text-sm font-medium text-gray-600">
 							Age:
 						</label>
 						<input
@@ -89,13 +103,6 @@ const RegistrationForm = () => {
 						</select>
 					</div>
 					<div className="text-center">
-						{/* <button
-							type="submit"
-							className="text-white text-center px-8 py-2 rounded-md button"
-						>
-							Register
-						</button> */}
-
                         <Web3Button
                             contractAddress="0x2721d46144702600fa700548B2E98D1d59896CAa"
                             action={(contract) => {
