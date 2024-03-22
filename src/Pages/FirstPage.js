3e3f33fb7ce86f66b3cf1aeadbeca54466b6e518
Dhanuja Thishakya Samaranayake <d.thishakya@gmail.com>
2024-03-22 23:04:45 +0530

    RegistrationSection fix errors

--- src/Pages/FirstPage.js
+++ src/Pages/FirstPage.js
@@ -2,7 +2,7 @@ import '../App.css';
 import AboutSection from '../Components/AboutSection';
 import InstructionSection from '../Components/InstructionSection';
 import PointSection from '../Components/PointSection';
-import RegistrationSection from '../Components/Registration';
+import RegistrationSection from '../Components/RegistrationSection';
 import WelcomeSection from '../Components/WelcomeSection';
 import React from 'react';
 
