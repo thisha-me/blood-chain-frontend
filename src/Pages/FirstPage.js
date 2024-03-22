81a7481b2e1092def8ca3a6093ffbaaf120328ce
Dhanuja Thishakya Samaranayake <d.thishakya@gmail.com>
2024-03-22 23:02:22 +0530

    PointSection error fix

--- src/Pages/FirstPage.js
+++ src/Pages/FirstPage.js
@@ -1,7 +1,7 @@
 import '../App.css';
 import AboutSection from '../Components/AboutSection';
 import InstructionSection from '../Components/InstructionSection';
-import PointSection from '../Components/Point';
+import PointSection from '../Components/PointSection';
 import RegistrationSection from '../Components/RegistrationSection';
 import WelcomeSection from '../Components/WelcomeSection';
 import React from 'react';
