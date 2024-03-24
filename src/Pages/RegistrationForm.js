a85caa21cc55d6270dd36fced1a8b2b000d30952
Dhanuja Thishakya Samaranayake <d.thishakya@gmail.com>
2024-03-24 17:16:42 +0530

    fix error on registration form

--- src/Pages/RegistrationForm.js
+++ src/Pages/RegistrationForm.js
@@ -1,18 +1,19 @@
+import React from "react";
 import { useState } from "react";
 import { Web3Button } from "@thirdweb-dev/react";
 
-const CONTRACT_ADDRESS ="0x53A1F65Ab31E7F971082947fb79D335C77549a9c";
+const CONTRACT_ADDRESS = "0x53A1F65Ab31E7F971082947fb79D335C77549a9c";
 
 const RegistrationForm = () => {
-    const [formData, setFormData] = useState({
-        username : '',
-        contactNum : '',
-		email : '',
-        age: '',
-        bloodType : ''
-    });
+	const [formData, setFormData] = useState({
+		username: '',
+		contactNum: '',
+		email: '',
+		age: '',
+		bloodType: ''
+	});
 
-    const handleChange = (e) => {
+	const handleChange = (e) => {
 		const { name, value } = e.target;
 		setFormData((prevData) => ({
 			...prevData,
@@ -20,17 +21,17 @@ const RegistrationForm = () => {
 		}));
 	};
 
-    // const [step, setStep] = useState(1);
+	// const [step, setStep] = useState(1);
 
-    const handleSubmit = (e) => {
-        // Logic for handling registration
-        e.preventDefault();
-        console.log('Registered!');
+	const handleSubmit = (e) => {
+		// Logic for handling registration
+		e.preventDefault();
+		console.log('Registered!');
 		console.log('Form submitted:', formData);
-    };
+	};
 
-    return (
-        <div className="flex items-center justify-center h-screen mt-16 mb-16">
+	return (
+		<div className="flex items-center justify-center h-screen mt-16 mb-16">
 			<div className="w-full max-w-xl mx-auto m-4 p-4 bg-white rounded-md shadow-lg">
 				<h1 className="text-3xl text-center mb-4">BloodChain Registration Form</h1>
 				<form onSubmit={handleSubmit}>
@@ -105,19 +106,19 @@ const RegistrationForm = () => {
 						</select>
 					</div>
 					<div className="text-center">
-                        <Web3Button
-                            contractAddress={CONTRACT_ADDRESS}
-                            action={(contract) => {
-                                contract.call("registerUser", [formData.username, formData.contactNum, formData.email, formData.age, formData.bloodType])
-                            }}
-                            >
-                            Register
-                        </Web3Button>
+						<Web3Button
+							contractAddress={CONTRACT_ADDRESS}
+							action={(contract) => {
+								contract.call("registerUser", [formData.username, formData.contactNum, formData.email, formData.age, formData.bloodType])
+							}}
+						>
+							Register
+						</Web3Button>
 					</div>
 				</form>
 			</div>
 		</div>
-    );
+	);
 };
 
 const bloodTypes = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
